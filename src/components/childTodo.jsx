9dbd138a72dd8edacce74110c22ac0bd99d79d21
philskay <theophiluskuyebi@gmail.com>
2022-09-23 17:21:44 +0100

    working on todo application

---
+++ src/components/childTodo.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+const Todo = (props) => {
+  const todos = props.todos;
+  const todo = props.todo;
+  const index = props.index;
+  const setTodos = props.setTodos;
+
+  const [editing, setEditing] = React.useState(false);
+  const [editingText, setEditingText] = React.useState("");
+
+  const handleDelete = (currentTodo, id) => {
+    let newTodos = todos.filter((_todo, _index) => {
+      if (_todo === currentTodo && id === _index) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    setTodos(newTodos);
+  };
+
+  const handleEdit = (todo) => {
+    setEditing(true);
+    setEditingText(todo);
+    console.log({ editing: todo });
+  };
+
+  const Update = (currentTodo, id) => {
+    let updatedTodos = todos.map((todo, index) => {
+      if (currentTodo === todo && index === id) {
+        return editingText;
+      } else {
+        return todo;
+      }
+    });
+
+    console.log({ updatedTodos });
+    setTodos(updatedTodos);
+    setEditing(false);
+  };
+
+  const handleChange = (e) => {
+    setEditingText(e.target.innerText);
+  };
+
+  return (
+    <div className="todo" key={index}>
+      <span
+        dangerouslySetInnerHTML={{ __html: todo }}
+        style={
+          editing
+            ? {
+                flexGrow: 1,
+                border: "2px solid grey",
+                padding: "5px 10px",
+                borderRadius: "10px",
+                marginRight: 10,
+                background: "#fff",
+                color: "#00000080",
+              }
+            : undefined
+        }
+        onKeyUp={editing ? handleChange : undefined}
+        className="label"
+        contentEditable={editing}
+      />
+      <div className="action-btn">
+        <button
+          className="edit"
+          onClick={editing ? () => Update(todo, index) : () => handleEdit(todo)}
+        >
+          {/* If editing the icon will change to update icons, otherwise the icon will be edit icon */}
+          {editing ? (
+            <span className="material-symbols-outlined">update</span>
+          ) : (
+            <span className="material-symbols-outlined">edit</span>
+          )}
+        </button>
+        <button className="delete" onClick={() => handleDelete(todo, index)}>
+          <span className="material-symbols-outlined">delete</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
--- src/components/todos.jsx
+++ src/components/todos.jsx
@@ -1,37 +1,21 @@
+import React from "react";
+import ChildTodo from "./childTodo";
+
 const Todos = (props) => {
   const todos = props.todos;
   const setTodos = props.setTodos;
-
-  const handleClick = (currentTodo, id) => {
-    let newTodos = todos.filter((_todo, _index) => {
-      if (_todo === currentTodo && id === _index) {
-        return false;
-      } else {
-        return true;
-      }
-    });
-
-    setTodos(newTodos);
-  };
 
   return (
     <div className="lists-of-todo">
       {todos.map((todo, index) => {
         return (
-          <div className="todo" key={index}>
-            <div className="label">{todo}</div>
-            <div className="action-btn">
-              <button className="edit">
-                <span className="material-symbols-outlined">edit</span>
-              </button>
-              <button
-                className="delete"
-                onClick={() => handleClick(todo, index)}
-              >
-                <span className="material-symbols-outlined">delete</span>
-              </button>
-            </div>
-          </div>
+          <ChildTodo
+            todos={todos}
+            todo={todo}
+            index={index}
+            setTodos={setTodos}
+            key={index}
+          />
         );
       })}
     </div>
